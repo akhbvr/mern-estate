f1163b9b1c9b1f4c98aaf306661af53222e61008
akhbvr <akhbvr@gmail.com>
2023-11-17 12:08:03 +0400

    complete sign up page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -18,8 +18,8 @@ export default function Header() {
           <FaSearch className='text-slate-600'/>
         </form>
         <ul className='flex gap-4'>
-          <Link to="/"><li className='hiden sm:inline text-slate-700 hover:underline cursor-pointer'>Home</li></Link>
-          <Link to="/about"><li className='hiden sm:inline text-slate-700 hover:underline cursor-pointer'>About</li></Link>
+          <Link to="/"><li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>Home</li></Link>
+          <Link to="/about"><li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>About</li></Link>
           <Link to="/sign-in"><li className='text-slate-700 hover:underline cursor-pointer'>Sign in</li></Link>
         </ul>
 
