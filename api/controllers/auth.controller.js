967b738476f85f68f5970a0eb7d5d15fca550dd9
akhbvr <akhbvr@gmail.com>
2023-11-20 15:17:35 +0400

    add sign out functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -61,3 +61,12 @@ export const google = async (req, res, next) => {
         next(error)
     }
 }
+
+export const signOut = (req, res, next) => {
+    try {
+        res.clearCookie('access_token')
+        res.status(200).json('User has ben logged out!')
+    } catch (error) {
+        next(error)
+    }
+}
